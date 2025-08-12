822dccbfca5950ff23c76be6c25e28cb5a6ffc35
Creighton Gorai <cgorai@Creightons-MacBook-Pro.local>
2025-08-12 09:34:17 +0900

    Adding stop and reset js functions

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const startEl = document.getElementById("start");
 const timerEl = document.getElementById("timer");
+const resetEl = document.getElementById("reset");
+const stopEl = document.getElementById("stop");
 
 let interval;
 let timeLeft = 1500;
@@ -25,4 +27,16 @@ function startTimer() {
     }, 1000);
 }
 
+function resetTimer() {
+    clearInterval(interval);
+    timeLeft = 1500;
+    updateTimer();
+}
+
+function stopTimer() {
+    clearInterval(interval);
+}
+
 startEl.addEventListener("click", startTimer);
+resetEl.addEventListener("click", resetTimer);
+stopEl.addEventListener("click", stopTimer);
