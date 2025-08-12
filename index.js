709204b547c88e21a295f781c1a31a29595bb5b4
Creighton Gorai <cgorai@Creightons-MacBook-Pro.local>
2025-08-12 09:21:23 +0900

    Adding start timer and count down

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const startEl = document.getElementById("start");
+const timerEl = document.getElementById("timer");
+
+let interval;
+let timeLeft = 1500;
+
+function updateTimer() {
+    let minutes = Math.floor(timeLeft / 60);
+    let seconds = timeLeft % 60;
+    let formattedTime = `${minutes.toString()}:${seconds.toString()}`;
+
+    timerEl.innerHTML = formattedTime;
+}
+
+function startTimer() {
+    interval = setInterval(() => {
+        timeLeft--;
+        updateTimer();
+        if (timeleft == 0) {
+            clearInterval(interval);
+            alert("Time is Up!");
+            timeLeft = 1500;
+            updateTimer();
+        }
+    }, 1000);
+}
+
+startEl.addEventListener("click", startTimer);
